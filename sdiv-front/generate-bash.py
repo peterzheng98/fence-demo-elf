a642b9e248760c00a327bf8a48bc687dc4b6eb3a
Wenxin Zheng <peterzheng98@sjtu.edu.cn>
2020-11-16 11:31:42 +0800

    script: add bash generator

---
+++ sdiv-front/generate-bash.py
@@ -0,0 +1,22 @@
+import sys
+
+bash_build_cmd = '''
+python3 generate-bash.py {} > test{}.s
+make elf
+'''
+
+bash_runtime_cmd = '''
+./data > runtime.output.{}.{}
+echo -e "{}/{}: Running sdiv {} at times {}: Easy report: \c"
+tail -1 runtime.output.{}.{}
+'''
+
+max_sdiv_cnt = int(sys.argv[1])
+each_run_time = int(sys.argv[2])
+output_str = ''
+for cur_sdiv in range(max_sdiv_cnt + 1):
+    output_str += bash_build_cmd.format(cur_sdiv, cur_sdiv)
+    for run_time in range(each_run_time):
+        output_str += bash_runtime_cmd.format(cur_sdiv, run_time, cur_sdiv, run_time, cur_sdiv, run_time, cur_sdiv, run_time)
+
+print(output_str)
